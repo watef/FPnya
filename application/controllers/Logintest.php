883198b7da59555cea30247c043a382ad613b7ca
watef <lulukwatef76@gmail.com>
2018-05-19 10:46:29 +0700

    v8

--- application/controllers/Logintest.php
+++ application/controllers/Logintest.php
@@ -1,11 +1,14 @@
 <?php
-
-
+/*
+ * @assert
+ */
 class Login extends CI_Controller{
 
 	function __construct(){
 		parent::__construct();
 		$this->load->model('m_login');
+                $this->load->library('unit_test');
+
 
 	}
 
